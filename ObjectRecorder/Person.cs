94f3bcc2ac2be23cded2f16148f39a74a60c120b
Darker Laptop <hunmate0704@gmail.com>
2023-12-06 18:10:26 +0100

    GetMethods, LINQ on it to get the specified Parse method.

---
+++ ObjectRecorder/Person.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectRecorder
+{
+    internal class Person/* : /*IComparable<Person>*/
+    {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Job { get; set; }
+        //public bool IsMan { get; set; }
+        public Person()
+        {
+
+        }
+        public Person(string name, int age, string job)
+        {
+            Name = name;
+            Age = age;
+            Job = job;
+        }
+
+        //public void ChangeIsMan()
+        //{
+        //    IsMan = !IsMan;
+        //}
+
+        //public int Highest<T>()
+        //{
+        //    if (Name.Length > Age)
+        //    {
+        //        return Name.Length;
+        //    }
+        //    else
+        //    {
+        //        return Age;
+        //    }
+        //}
+
+        //public int CompareTo(Person other)
+        //{
+        //    if (Age > other.Age)
+        //    {
+        //        return 1;
+        //    }
+        //    else if (Age < other.Age)
+        //    {
+        //        return -1;
+        //    }
+        //    else
+        //    {
+        //        if (IsMan)
+        //        {
+        //            return -1;
+        //        }
+        //        else if (other.IsMan)
+        //        {
+        //            return 1;
+        //        }
+        //        return 0;
+        //    }
+        //}
+    }
+}
--- ObjectRecorder/Program.cs
+++ ObjectRecorder/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ObjectRecorder
 {
@@ -6,7 +8,64 @@ namespace ObjectRecorder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person p = RecordObject<Person>();
+            ;
+        }
+
+        static T RecordObject<T>()
+        {
+            Type t = typeof(T);
+            object instance = Activator.CreateInstance(t);
+            foreach (var item in t.GetProperties())
+            {
+                Console.WriteLine($"Enter {item.Name} value:");
+                string result = Console.ReadLine();
+
+                if (item.PropertyType == typeof(string))
+                {
+                    item.SetValue(instance, result);
+                }
+                else
+                {
+                    Type propType = item.PropertyType;
+                    var ParseMethod = propType.GetMethods().First(t => t.Name.Contains("Parse"));
+                    var converted = ParseMethod.Invoke(null, new object[] { result });
+                    item.SetValue(instance, converted);
+
+                }
+
+
+
+
+            }
+            return (T)instance;
+        }
+
+        static void CollectionTraverser<T>(IEnumerable<T> collection)
+        {
+            Type t = typeof(T);
+            foreach (var item in t.GetProperties())
+            {
+                Console.Write(item.Name.Length > 7 ? item.Name.Substring(0, 7) : item.Name + "\t");
+
+            }
+            Console.WriteLine();
+            foreach (var item in collection)
+            {
+                foreach (var prop in t.GetProperties())
+                {
+                    if (prop.GetValue(item).ToString().Length > 7)
+                    {
+                        Console.Write(prop.GetValue(item).ToString().Substring(0, 7) + "\t");
+                    }
+                    else
+                    {
+                        Console.Write(prop.GetValue(item) + "\t");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
         }
     }
 }
